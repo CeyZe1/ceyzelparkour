b225b013fb868dca723f207df99f8d88808ea736
Hagarashi8 <artem501137@gmail.com>
2025-03-29 10:59:14 +0600

    Добавил /kill

--- src/main/java/net/ceyzel/parkour/LobbyHub.java
+++ src/main/java/net/ceyzel/parkour/LobbyHub.java
@@ -8,6 +8,7 @@ import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.entity.Player;
 
 public class LobbyHub {
     public static void RegisterCommands(CeyZelParkour plugin) {
@@ -23,6 +24,11 @@ public class LobbyHub {
             var reg = commands.registrar();
             reg.register("hub", lobby);
             reg.register("lobby", lobby);
+            reg.register("kill", ((commandSourceStack, args) -> {
+                if (commandSourceStack.getExecutor() instanceof Player executor){
+                    executor.setHealth(0);
+                }
+            }));
         });
     }
 }
